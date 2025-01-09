1b84ca812a147e7a5aeeb311e21b423b80bc68e7
M.VINOTHKUMAR <166404677+M-VINOTHKUMAR@users.noreply.github.com>
2025-01-09 08:26:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Total Decoding Messages/total-decoding-messages.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String s = br.readLine().trim();
+            Solution ob = new Solution();
+            int ans = ob.countWays(s);
+            out.println(ans);
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int countWays(String s) {
+        // code here
+        int n=s.length();
+        int dp[]=new int[n+1];
+        if(n==0 || s.charAt(0)=='0')
+          return 0;
+          
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            if(s.charAt(i-1)!='0')
+               dp[i]+=dp[i-1];
+            int val=Integer.parseInt(s.substring(i-2,i));
+            if(val>=10 && val<=26)
+              dp[i]+=dp[i-2];
+        }
+        return dp[n];
+    }
+}
